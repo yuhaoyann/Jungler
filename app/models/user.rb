4b8c39868ed15af2af19a44bc5f881a5f8359e9b
yuhaoyann <yuhaoyann@gmail.com>
2022-01-17 19:03:47 -0500

    add validation for user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
